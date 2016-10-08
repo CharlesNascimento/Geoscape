0bd06c0ad324b8ef4b760a947658afed5c87ef3f
Everton Toyama <everton.toyama@gmail.com>
2016-10-08 20:20:57 -0300

    Lean Tween Funcionalidades 	a. Base 	b. Atração 	c. Impulsão 	d. Cola

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private static Player instance;
+	public static Player Instance {
+		get {
+			if(instance == null) {
+				instance = GameObject.FindObjectOfType<Player>();
+				if(instance == null) {
+					Debug.LogWarning("Nenhum Player do tipo GameManager foi encontrado");
+				}
+			}
+
+			return instance;
+		}
+	}
+
+	public Vector3 GetPosition() {
+		return transform.position;
+	}
+}

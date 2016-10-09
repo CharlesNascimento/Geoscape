9d8d1b60fb2312fa39c1bfa730a9230099629879
Everton Toyama <everton.toyama@gmail.com>
2016-10-09 03:11:48 -0300

    Angulo do impulse

--- Assets/Scripts/Elements/ImpulseElement.cs
+++ Assets/Scripts/Elements/ImpulseElement.cs
@@ -3,12 +3,21 @@ using System.Collections;
 
 public class ImpulseElement : MonoBehaviour {
 
+	public float angle;
+
 	public float force;
 
 	void OnCollisionEnter2D(Collision2D coll) {
 		if(coll.gameObject.CompareTag("Player")) {
-			coll.transform.GetComponent<Rigidbody2D>().AddForce(Vector2.up * force, ForceMode2D.Impulse);
+			coll.transform.GetComponent<Rigidbody2D>().AddForce(DegreeToVector2(angle) * force, ForceMode2D.Impulse);
 		}
 	}
 
+	public static Vector2 RadianToVector2(float radian) {
+    	return new Vector2(Mathf.Cos(radian), Mathf.Sin(radian));
+ 	}
+  
+ 	public static Vector2 DegreeToVector2(float degree) {
+    	return RadianToVector2(degree * Mathf.Deg2Rad);
+ 	}
 }

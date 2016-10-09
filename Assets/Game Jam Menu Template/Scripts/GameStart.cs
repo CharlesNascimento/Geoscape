bae6981a9a26f7079ca2b7522367184b1940c722
Kansus <charles.si.ufs@gmail.com>
2016-10-09 18:55:30 -0300

    Tela de história

---
+++ Assets/Game Jam Menu Template/Scripts/GameStart.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level01");
+    }
+}

b1322c6a132b96076ed73c144c12c863312f6e44
Walan Marcel <walan.dn@gmail.com>
2016-10-09 18:49:04 -0300

    Menu

---
+++ Assets/Scripts/DeathZone.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathZone : MonoBehaviour {
+
+	//public bool destroySelfOnImpact = false;
+	//public float delayBeforeDestroy = 0.0f;
+	public GameObject explosionPrefab;
+
+	void OnTriggerEnter(Collider collision) {
+
+		Debug.Log("Sucesso");
+
+		if (explosionPrefab != null) {
+			Instantiate (explosionPrefab, transform.position, transform.rotation);
+		}
+
+		GameManager.Instance.GameOver();
+
+
+	}
+
+
+}
